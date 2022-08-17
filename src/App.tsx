6be8fd0d95580235bf8ad180506a73972a3ca41e
akinus <42932934+akimo10@users.noreply.github.com>
2022-08-17 11:46:16 +0900

    型指定が上手くいかないアプリ例を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import axios from "axios";
+import { useState } from "react";
 import { Practice1 } from "./practices/Practice1";
 import { Practice2 } from "./practices/Practice2";
 import { Practice3 } from "./practices/Practice3";
 import { Practice4 } from "./practices/Practice4";
+import { Todo } from "./Todo";
 
 import "./styles.css";
 
 export default function App() {
+  const [todos, setTodos] = useState<any>([]);
   const onClickFetchData = () => {
     axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
       console.log(res.data);
+      setTodos(res.data);
     });
   };
   return (
@@ -20,6 +24,9 @@ export default function App() {
       <Practice4 />
       <br />
       <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo title={todo.title} userid={todo.userid} />
+      ))}
     </div>
   );
 }
