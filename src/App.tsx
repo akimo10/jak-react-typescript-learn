b68db4b2c738eaafd18b4a80bc5043095d2d3f5c
akinus <42932934+akimo10@users.noreply.github.com>
2022-08-17 12:22:11 +0900

    型指定の共通化(typesフォルダへ移管)

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,9 @@ import { Practice2 } from "./practices/Practice2";
 import { Practice3 } from "./practices/Practice3";
 import { Practice4 } from "./practices/Practice4";
 import { Todo } from "./Todo";
+import { TodoType } from "./types/todo";
 
 import "./styles.css";
-
-type TodoType = {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-};
 
 export default function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -34,6 +28,7 @@ export default function App() {
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
         <Todo
+          key={todo.id}
           title={todo.title}
           userId={todo.userId}
           completed={todo.completed}
--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,11 +1,18 @@
-type TodoType = {
-  userId: number;
-  title: string;
-  //?をつけることで必須ではないことを明示できる
-  completed?: boolean;
-};
+import { TodoType } from "./types/todo";
 
-export const Todo = (props: TodoType) => {
+// type TodoType = {
+//   userId: number;
+//   title: string;
+//   //?をつけることで必須ではないことを明示できる
+//   completed?: boolean;
+// };
+
+//Pick:共通型指定の中から、指定の型をピックアップ(ホワイトリスト)
+//Omit:共通型指定の中から、不要なものを選択し除外(ブラックリスト)
+export const Todo = (
+  // props: Pick<TodoType, "userId" | "title" | "completed">
+  props: Omit<TodoType, "id">
+) => {
   const { title, userId, completed = false } = props;
   const completedMark = completed ? "[完]" : "[未]";
   return <p>{`${completedMark} ${title}(ユーザー:${userId})`}</p>;
