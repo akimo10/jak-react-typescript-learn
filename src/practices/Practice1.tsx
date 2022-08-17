2540b1fdf67f0554eeba2bda22330fa6a1cec388
akinus <42932934+akimo10@users.noreply.github.com>
2022-08-17 10:38:25 +0900

    引数の型指定確認

---
+++ src/practices/Practice1.tsx
@@ -0,0 +1,17 @@
+export const Practice1 = () => {
+  const calcTotalFee = (num: number) => {
+    const total = num * 1.1;
+    console.log(total);
+  };
+  const onClickPractice = () => {
+    //JSだと暗黙的に型変換してくれるのでエラーは出ない...TSは？
+    // calcTotalFee("1,000");//TSで引数型指定しているとエラーがでてくれる
+    calcTotalFee(1000);
+  };
+  return (
+    <div>
+      <p>練習問題：引数の型指定</p>
+      <button onClick={onClickPractice}>練習問題1を実行</button>
+    </div>
+  );
+};
