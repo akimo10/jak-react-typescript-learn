e3852e3fd0491037deb087cd6808d8b5e99106f4
akinus <42932934+akimo10@users.noreply.github.com>
2022-08-17 11:05:51 +0900

    typeScript実践編準備

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { Practice1 } from "./practices/Practice1";
 import { Practice2 } from "./practices/Practice2";
 import { Practice3 } from "./practices/Practice3";
@@ -6,12 +7,19 @@ import { Practice4 } from "./practices/Practice4";
 import "./styles.css";
 
 export default function App() {
+  const onClickFetchData = () => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      console.log(res.data);
+    });
+  };
   return (
     <div className="App">
       <Practice1 />
       <Practice2 />
       <Practice3 />
       <Practice4 />
+      <br />
+      <button onClick={onClickFetchData}>データ取得</button>
     </div>
   );
 }
